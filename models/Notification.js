7f92f6c7ccfcffafcd8e20aff46fd909720276e5
NATER MBASHAU <nmbashau@gmail.com>
2026-09-12 19:05:01 +0100

    Update Notification schema for global announcements
    Modified notification schema to allow global announcements without a user reference. Added new fields for global visibility and refined existing fields.

--- models/Notification.js
+++ models/Notification.js
@@ -4,8 +4,19 @@ const notificationSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
+    required: false, // Changed to false so Global Announcements can be saved
     index: true
+  },
+  
+  isGlobal: {
+    type: Boolean,
+    default: false, // True means ALL users will see this
+    index: true
+  },
+
+  image: {
+    type: String,
+    default: null // Explicitly allows Cloudinary image URLs
   },
   
   title: {
@@ -20,7 +31,7 @@ const notificationSchema = new mongoose.Schema({
   
   type: {
     type: String,
-    enum: ['transaction', 'security', 'system', 'promotion', 'kyc', 'support', 'merchant', 'referral'],
+    enum: ['transaction', 'security', 'system', 'promotion', 'kyc', 'support', 'merchant', 'referral', 'info'],
     default: 'system'
   },
   
@@ -30,48 +41,23 @@ const notificationSchema = new mongoose.Schema({
     default: 'medium'
   },
   
-  // Action link
-  actionLink: {
-    type: String,
-    default: null
-  },
+  actionLink: { type: String, default: null },
+  actionLabel: { type: String, default: null },
   
-  actionLabel: {
-    type: String,
-    default: null
-  },
-  
-  // Status
-  isRead: {
-    type: Boolean,
-    default: false
-  },
-  
+  isRead: { type: Boolean, default: false },
   readAt: Date,
   
-  // Channels
   channels: [{
     type: String,
     enum: ['in_app', 'email', 'sms'],
     default: ['in_app']
   }],
   
-  // Delivery status
-  emailSent: {
-    type: Boolean,
-    default: false
-  },
-  
+  emailSent: { type: Boolean, default: false },
   emailSentAt: Date,
-  
-  smsSent: {
-    type: Boolean,
-    default: false
-  },
-  
+  smsSent: { type: Boolean, default: false },
   smsSentAt: Date,
   
-  // Metadata
   metadata: {
     type: Map,
     of: mongoose.Schema.Types.Mixed,
@@ -90,40 +76,6 @@ notificationSchema.methods.markAsRead = function() {
   this.isRead = true;
   this.readAt = new Date();
   return this.save();
-};
-
-notificationSchema.methods.markEmailSent = function() {
-  this.emailSent = true;
-  this.emailSentAt = new Date();
-  return this.save();
-};
-
-notificationSchema.methods.markSmsSent = function() {
-  this.smsSent = true;
-  this.smsSentAt = new Date();
-  return this.save();
-};
-
-// Static methods
-notificationSchema.statics.findByUser = function(userId, options = {}) {
-  const query = { user: userId };
-  if (options.unreadOnly) query.isRead = false;
-  if (options.type) query.type = options.type;
-  
-  return this.find(query)
-    .sort({ createdAt: -1 })
-    .limit(options.limit || 50);
-};
-
-notificationSchema.statics.findUnreadCount = function(userId) {
-  return this.countDocuments({ user: userId, isRead: false });
-};
-
-notificationSchema.statics.markAllAsRead = function(userId) {
-  return this.updateMany(
-    { user: userId, isRead: false },
-    { isRead: true, readAt: new Date() }
-  );
 };
 
 module.exports = mongoose.model('Notification', notificationSchema);
